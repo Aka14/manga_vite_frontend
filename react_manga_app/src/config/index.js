39efb229752db693bdecc687d3d1aff84aeedbc5
Aka14 <41841642+Aka14@users.noreply.github.com>
2026-04-15 03:11:26 -0400

    changed to deployed endpoints

--- react_manga_app/src/config/index.js
+++ react_manga_app/src/config/index.js
@@ -1,2 +1,3 @@
 export const BASE_URL = "https://www.mangapill.com";
-export const API_URL = "http://127.0.0.1:8000/api/";
+// export const API_URL = "http://127.0.0.1:8000/api/";
+export const API_URL = "https://manga-tracking-app.onrender.com/api/";
