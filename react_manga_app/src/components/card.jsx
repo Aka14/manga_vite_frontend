c64e32cfa1476426cc55e24e9fc269ff050db4bd
Aka14 <41841642+Aka14@users.noreply.github.com>
2026-04-15 16:11:02 -0400

    fixed link

--- react_manga_app/src/components/card.jsx
+++ react_manga_app/src/components/card.jsx
@@ -10,27 +10,27 @@ const Card = ({
   onRightClick,
 }) => {
   console.log(manga_name, cover, current_chapter, chapter_url, latest_url);
-  
+
   return (
-      <div
-        className="w-[200px] flex flex-col gap-.5 max-w-sm overflow-hidden mt-auto"
-        onContextMenu={onRightClick}
+    <div
+      className="w-[200px] flex flex-col gap-.5 max-w-sm overflow-hidden mt-auto"
+      onContextMenu={onRightClick}
+    >
+      <p className="font-bold text-xl">{manga_name}</p>
+      <p className="text-md">{current_chapter}</p>
+      <a
+        href={`${BASE_URL + chapter_url}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        onClick={(e) => e.stopPropagation()}
       >
-        <p className="font-bold text-xl">{manga_name}</p>
-        <p className="text-md">{current_chapter}</p>
-        <a
-          href={`${chapter_url}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <img
-            className="h-85 rounded-2xl py-2 object-cover"
-            src={cover}
-            alt={`Cover image for ${manga_name}`}
-          />
-        </a>
-      </div>
+        <img
+          className="h-85 rounded-2xl py-2 object-cover"
+          src={cover}
+          alt={`Cover image for ${manga_name}`}
+        />
+      </a>
+    </div>
   );
 };
 
